bbce6477851545583127d6b5bd4019ba4df1ae8a
me <me@debian>
2017-02-27 04:04:05 -1000

    cam read, save, replay

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 
 #include <opencv2/opencv.hpp>
+#include <unistd.h>
 
 using namespace cv;
 using namespace std;
@@ -14,7 +15,7 @@ using namespace std;
 int main( int argc, char** argv )
 
 {
-
+	/*
 	Mat image;
 
 	image = imread( argv[1], 1 );
@@ -49,7 +50,57 @@ int main( int argc, char** argv )
 	}catch (cv::Exception& ex){
 		fprintf(stderr, "exception saving image: %s\n", ex.what());
 	}
+*/
 
+	VideoCapture cap(0); // open the default camera
+	if(!cap.isOpened())  // check if we succeeded
+		return -1;
+
+	vector<int> compression_params;
+	compression_params.push_back(IMWRITE_PNG_COMPRESSION);
+	compression_params.push_back(0);
+
+
+
+	Mat edges;
+	namedWindow("edges",CV_WINDOW_AUTOSIZE);
+	int i = 0;
+	for(;;)
+	{
+		Mat frame;
+		cap >> frame; // get a new frame from camera
+
+		if (frame.empty()) break;
+		imshow("edges", frame);
+		char name[20];
+		sprintf(name,"img%d.png",i);
+		try{
+			imwrite(name, frame, compression_params);
+		}catch (cv::Exception& ex){
+			fprintf(stderr, "exception saving image: %s\n", ex.what());
+		}
+		i++;
+		if(waitKey(10) == 27) break;
+		//if (i>50) break;
+	}
+	//cap.release();
+
+	i = 0;
+	namedWindow("edges2",CV_WINDOW_AUTOSIZE);
+	for (;;)
+	{
+		Mat frame2;
+
+		char name[20];
+		sprintf(name,"img%d.png",i);
+		frame2 = imread(name,1);
+		i++;
+		if (frame2.data == NULL)
+			break;
+		imshow("edges2", frame2);
+		if(waitKey(10) == 27) break;
+		usleep(30000);
+	}
 	return 0;
 
 }
